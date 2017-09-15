b0d6386b56787fe9fe6599f298303e016095bdb8
Tobias Munk <schmunk@usrbin.de>
2017-09-15 20:22:04 +0200

    added application source-code

---
+++ src/config/main.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2017 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use codemix\streamlog\Target;
+use yii\gii\Module;
+
+return [
+    'id' => 'app',
+    'basePath' => realpath(__DIR__.'/..'),
+    'runtimePath' => realpath(__DIR__.'/../../runtime'),
+    'aliases' => [
+        '@bower' => '/app/pkg/node_modules',
+        '@yii/gii' => '/app/pkg/vendor/yiisoft/yii2-gii',
+    ],
+    'bootstrap' => [
+    ],
+    'logger' => [
+        'flushInterval' => 1,
+        'targets' => [
+            [
+                'class' => Target::class,
+                'url' => 'php://stdout',
+                #'levels' => ['error', 'warning'],
+                'logVars' => [],
+                #'replaceNewline' => ''
+            ],
+        ],
+    ],
+    'modules' => [
+        'gii' => [
+            'class' => Module::class,
+            'allowedIPs' => ['*'],
+        ],
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'request' => [
+            'cookieValidationKey' => 'VERY_SECRET'
+        ],
+    ],
+];
---
+++ src/views/layouts/main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2017 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+?>
+
+<?= $content ?>
